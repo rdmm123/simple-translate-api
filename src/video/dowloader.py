c686b8bc4fa5621785038a8842fb41ba36b361d5
Robert Martinez <rdmarenco@uninorte.edu.co>
2024-11-27 20:24:07 -0400

    Use python ffmpeg for compression, refactor in downlaoder

--- src/video/dowloader.py
+++ src/video/dowloader.py
@@ -1,13 +1,15 @@
 
-import os
 import re
-import subprocess
+import ffmpeg
+from pathlib import Path
 
 from playwright.async_api import Browser
 from yt_dlp import YoutubeDL
 from loguru import logger
 
-LOGIN_URL = 'https://autismpartnershipfoundation.org/all-courses/my-account/'
+# TODO: Download, compress and upload chunk by chunk
+
+LOGIN_URL = 'https://autismpartnershipfoundation.org/log-in/'
 VIMEO_ID_URL = 'https://vimeo.com/{id}'
 
 class VideoNotFound(Exception):
@@ -21,74 +23,75 @@ def get_id_video(url: str):
     else:
         return None  # If the ID is not found
 
-
-def download_video(id: int):
-    logger.info("downloading video")
+def download_video(id: int) -> Path:
     #Path where the downloaded video will be saved
-    download_path = 'VideosVimeo/'
-    os.makedirs(download_path, exist_ok=True)
+    download_dir = Path('/tmp') # TODO: Replace with TemporaryDirectory
+    download_dir.mkdir(exist_ok=True)
 
     url = VIMEO_ID_URL.format(id=id)
+    logger.info(f"Downloading video at {url}")
 
-    logger.info("URl original video vimeo: ",url)
+    download_path = download_dir / 'video.mp4'
+    download_path.unlink(missing_ok=True)
     #Setting yt-dlp to get video title
     ydl_opts = {
         'format': 'bv+ba',
-        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'), #Use the original title of the video
-        'quiet': True,  #Suppress output of download process
+        'outtmpl': str(download_path),
+        'quiet': True,
     }
 
-    #Download the video using yt-dlp
     with YoutubeDL(ydl_opts) as ydl:
         logger.info(f"{ydl}")
-        info_dict = ydl.extract_info(url, download=False)  # We only extract the information without downloading it yet
-        video_title = info_dict.get('title', None)  #get the video title
-        print(f"Video descargado con el título: {video_title}")
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None)
+        logger.info(f"Video found, downloading: {video_title}")
 
         logger.info(f"downloading {url}")
         #Download the video now
         ydl.download([url])
 
-    # path of the downloaded video
-    input_filename = os.path.join(download_path, f'{video_title}.mp4')
+    return compress_video(download_path)
 
-    # path of the compressed file
-    compressed_filename = os.path.join(download_path, f'{video_title}_comprimido.mp4')
+def compress_video(path: Path) -> Path:
+    try:
+        stream = ffmpeg.input(str(path))
 
-    logger.info('downlaod complete. compressing video')
-    # ffmpeg settings to download the compressed video
-    ffmpeg_command = [
-        'ffmpeg',
-        '-i', input_filename,                 # input file
-        '-c:v', 'libx264',                    # H.264 video codec
-        '-crf', '23',                         # Video quality (adjustable)
-        '-preset', 'medium',                  # Compression speed preset
-        '-c:a', 'aac',                        # AAC audio codec
-        '-b:a', '128k',                       # Audio bit rate
-        compressed_filename                   # compressed output file
-    ]
+        output_path = path.parent / f'compressed_{path.name}'
+        output_path.unlink(missing_ok=True)
 
-    #Run FFmpeg to compress the video
-    subprocess.run(ffmpeg_command)
-    logger.info(f"Video comprimido guardado como: {compressed_filename}")
+        logger.info(f"Compressing {path} into {output_path}")
+        stream = ffmpeg.output(stream, str(output_path),
+            vcodec='libx264',
+            crf=23,
+            preset='medium',
+            acodec='aac',
+            audio_bitrate='128k'
+        )
 
-async def download_from_url(browser: Browser, user: str, passw: str, url: str):
+        ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
+
+        return output_path
+    except ffmpeg.Error as e:
+        logger.error('An error occurred while compressing the video: ', e.stderr.decode())
+        raise
+
+async def download_from_url(browser: Browser, user: str, passw: str, url: str) -> Path:
     ctx = await browser.new_context()
     page = await ctx.new_page()
 
     logger.info("logging in")
     #the first URL (the login page)
-    await page.goto(LOGIN_URL)
+    await page.goto(LOGIN_URL, wait_until='domcontentloaded')
 
+    await page.wait_for_timeout(1_000)
     #Complete the login form
-
-    await page.fill('input[name="username"]', user)
-    await page.fill('input[name="password"]', passw)
-    await page.click('button[type="submit"]')
+    await page.fill('input[name="log"]', user)
+    await page.fill('input[name="pwd"]', passw)
+    await page.click('input[type="submit"]')
 
     logger.info("log in succesful, going to video url")
 
-    await page.goto(url)
+    await page.goto(url, wait_until='domcontentloaded')
 
     #Search for the src selector that contains the vimeo.player of the video
     iframe = await page.query_selector("iframe[src*='vimeo.com']")
@@ -97,8 +100,8 @@ async def download_from_url(browser: Browser, user: str, passw: str, url: str):
         raise VideoNotFound(f'Vimeo iframe not found in page {url}')
 
     video_Link = await iframe.get_attribute("src")
-    browser.close()
+    await browser.close()
     logger.info("getting video id")
     id = get_id_video(video_Link)
     logger.info(f"video id = {id}")
-    download_video(id)
+    return download_video(id)
